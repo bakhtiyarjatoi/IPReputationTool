306c05e53c9dbca9c69d61f05a9f8ca1c8e14896
bakhtiyarjatoi <khankhanbakhtiyar@gmail.com>
2024-10-03 17:35:11 -0400

    Fix duplicate data issue in scanning.py and other files

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import tkinter as tk
 from ui_components import IPReputationToolUI
 
@@ -6,12 +5,3 @@ if __name__ == "__main__":
     root = tk.Tk()
     app = IPReputationToolUI(root)
     root.mainloop()
-=======
-import tkinter as tk
-from ui_components import IPReputationToolUI
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = IPReputationToolUI(root)
-    root.mainloop()
->>>>>>> f9f7ac290c5788dc5240a67e20845d2152b3fc88
